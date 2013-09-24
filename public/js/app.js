9be5763d1212489dc8afa91cfc71981b8249fc87
Max Nanis <max@maxnanis.com>
2013-09-23 23:52:51 -0700

    Watch while idle in a tab

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,19 @@
 $(function() {
     var socket = io.connect('/');
+
     socket.on('breaking_news', function (data) {
+        console.log('breaking : ', data);
         var news = data.news;
+        document.title = news.text;
+
         $('.news-content').text(news.text);
         $('.location-content').text(news.locations);
     });
+
     socket.on('tweet', function (data) {
         var validatedTweet = data.tweet;
+        document.title = "@"+ validatedTweet.user.screen_name + " :: "+ validatedTweet.text;
+
         if(validatedTweet.data.coordinates) {
             var coords = new google.maps.LatLng(validatedTweet.data.coordinates.coordinates[1], validatedTweet.data.coordinates.coordinates[0]);
             map.panTo(coords);
@@ -14,41 +21,37 @@ $(function() {
 
         var template = _.template($("#tweet-template").html());
         $(".tweet-container").fadeOut(function() {
-            $(this).html(template({img: validatedTweet.user.profile_image_url, 
-                                   text: validatedTweet.text, 
+            $(this).html(template({img: validatedTweet.user.profile_image_url,
+                                   text: validatedTweet.text,
                                    screen_name: validatedTweet.user.screen_name,
                                    reason: validatedTweet.reason}))
                     .fadeIn();
         });
-
     });
-    
+
     var center;
     if(map_location.length == 2) {
         center = new google.maps.LatLng(map_location[1], map_location[0]);
     } else {
-          codeAddress();
+        codeAddress();
     }
+
     var mapOptions = {
       center: center,
       zoom: 14,
       disableDefaultUI: true,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
+    function codeAddress() {
+        var geocoder = new google.maps.Geocoder(),
+            address = $('.location-content').text();
 
-
-      function codeAddress() {
-      var geocoder = new google.maps.Geocoder();;
-        var address = $('.location-content').text();
         geocoder.geocode( { 'address': address}, function(results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            map.panTo(results[0].geometry.location);
-          }
+            if (status == google.maps.GeocoderStatus.OK) {
+                map.panTo(results[0].geometry.location);
+            }
         });
-      }
-
-
+    }
 });
